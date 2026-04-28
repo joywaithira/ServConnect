ddabb7c85d425c2c90cd277d83609c2bcc4563c7
joywaithira <waithiramaryjoy2@gmail.com>
2026-04-28 10:26:10 +0300

    initial commit

---
+++ app/src/main/java/com/maryjoy/servconnect/screens/navigation/AppNavHost.kt
@@ -0,0 +1,40 @@
+package com.maryjoy.servconnect.screens.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.maryjoy.servconnect.screens.onboarding.LoginScreen
+import com.maryjoy.servconnect.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUT_SPLASH
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController)
+        }
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController)
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/maryjoy/servconnect/screens/navigation/Routes.kt
@@ -0,0 +1,5 @@
+package com.maryjoy.servconnect.screens.navigation
+
+const val ROUT_SPLASH = "splash"
+const val ROUT_LOGIN = "login"
+const val ROUT_REGISTRATION = "registration"
